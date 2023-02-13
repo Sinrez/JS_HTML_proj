a7cd7b3ea93a4bfba2a05709c7a8468885f05d88
sintez <madsint@gmail.com>
2023-02-13 16:17:17 +0300

    new

---
+++ JS_intens/scripts2.js
@@ -0,0 +1,24 @@
+// const str1 = 'I\'m OK'
+// const str2 = "Он сказал \"Привет\""
+// const str3 = `I\'m say ${str1}`
+
+// console.log(str1)
+// console.log(str2)
+// console.log(str3)
+
+// const str4 = "I'm say..."
+
+//NUMBER
+// const num1 = 75
+// alert(num1)
+
+const obj = {
+    name: "RRR",
+    age: 100500,
+    isHappy: true
+}
+console.log(obj.name)
+console.log(obj['age'])
+
+obj.job = 'Google'
+console.log(obj)
